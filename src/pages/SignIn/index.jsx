4b899180e76a6c3a5cbee06da111ceaba1626080
JPagglo <joao.cruz@pagglo.com>
2024-05-22 16:42:26 -0300

    ChatScreen

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -9,12 +9,60 @@ import {
   Platform
 } from 'react-native';
 
+import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+
+
+
 export default function SignIn() {
+  const navigation = useNavigation();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const [type, setType] = useState(false);
+
+
+  function handleLogin(){
+    if(type){
+      //Cadastrar novo usuario
+      if(name === '' || email === '' || password === '') return;
+      auth().createUserWithEmailAndPassword(email,password)
+      .then((user)=>{
+        user.user.updateProfile({
+          displayName: name
+        })
+        .then(() =>{
+          navigation.goBack();
+        })
+        
+      })
+      .catch((error) =>{
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+    
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+      })
+    }else{
+      //Logar usuario
+      auth()
+      .signInWithEmailAndPassword(email,password)
+      .then (() =>{
+        navigation.goBack();
+      })
+      .catch((error) =>{    
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+      })
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -45,9 +93,12 @@ export default function SignIn() {
         onChangeText={text => setPassword(text)}
         placeholder="Digite sua senha?"
         placeholderTextColor="#9999b"
+        secureTextEntry={true}
       />
 
-      <TouchableOpacity style={[styles.buttonLogin, {backgroundColor: type ? "#F53745" : "#57DD86"}]}>
+      <TouchableOpacity style={[styles.buttonLogin, {backgroundColor: type ? "#F53745" : "#57DD86"}]}
+        onPress={handleLogin}
+      >
         <Text style={styles.buttonText}>
           {type ? "Cadastrar" : "Acessar"}
         </Text>
